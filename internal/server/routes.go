8b665f830f50515e7e1722807b5f2f67447900ba
nutcas3 <mauricee423@gmail.com>
2024-08-15 03:03:32 +0300

    Initial commit

---
+++ internal/server/routes.go
@@ -0,0 +1,63 @@
+package server
+
+import (
+	"net/http"
+
+	"fmt"
+	"log"
+	"time"
+
+	"github.com/gin-gonic/gin"
+
+	"nhooyr.io/websocket"
+)
+
+func (s *Server) RegisterRoutes() http.Handler {
+	r := gin.Default()
+
+	r.GET("/", s.HelloWorldHandler)
+
+	r.GET("/health", s.healthHandler)
+
+	r.GET("/websocket", s.websocketHandler)
+
+	return r
+}
+
+func (s *Server) HelloWorldHandler(c *gin.Context) {
+	resp := make(map[string]string)
+	resp["message"] = "Hello World"
+
+	c.JSON(http.StatusOK, resp)
+}
+
+func (s *Server) healthHandler(c *gin.Context) {
+	c.JSON(http.StatusOK, s.db.Health())
+}
+
+func (s *Server) websocketHandler(c *gin.Context) {
+	w := c.Writer
+	r := c.Request
+	socket, err := websocket.Accept(w, r, nil)
+
+	if err != nil {
+		log.Printf("could not open websocket: %v", err)
+		_, _ = w.Write([]byte("could not open websocket"))
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+
+	defer socket.Close(websocket.StatusGoingAway, "server closing websocket")
+
+	ctx := r.Context()
+	socketCtx := socket.CloseRead(ctx)
+
+	for {
+		payload := fmt.Sprintf("server timestamp: %d", time.Now().UnixNano())
+		err := socket.Write(socketCtx, websocket.MessageText, []byte(payload))
+		if err != nil {
+			break
+		}
+		time.Sleep(time.Second * 2)
+	}
+}
